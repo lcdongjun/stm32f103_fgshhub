8f3f847d382a409251a01d3d37f9af9a3d40ef2f
台式机 <2438511335@qq.com>
2025-05-13 23:26:15 +0800

    初次上传github

---
+++ 01_test/Core/Inc/main.h
@@ -0,0 +1,146 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+#include "stdlib.h"
+#include <string.h>
+#include <stdbool.h>
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
+#define BAT_LEV_Pin GPIO_PIN_1
+#define BAT_LEV_GPIO_Port GPIOA
+#define OLED_CS_Pin GPIO_PIN_2
+#define OLED_CS_GPIO_Port GPIOA
+#define OLED_DC_Pin GPIO_PIN_3
+#define OLED_DC_GPIO_Port GPIOA
+#define OLED_RST_Pin GPIO_PIN_4
+#define OLED_RST_GPIO_Port GPIOA
+#define OLED_SCK_Pin GPIO_PIN_5
+#define OLED_SCK_GPIO_Port GPIOA
+#define OLED_SDA_Pin GPIO_PIN_7
+#define OLED_SDA_GPIO_Port GPIOA
+#define KEY1_Pin GPIO_PIN_10
+#define KEY1_GPIO_Port GPIOB
+#define KEY2_Pin GPIO_PIN_11
+#define KEY2_GPIO_Port GPIOB
+#define POWER_Pin GPIO_PIN_12
+#define POWER_GPIO_Port GPIOB
+#define L1_Pin GPIO_PIN_13
+#define L1_GPIO_Port GPIOB
+#define L2_Pin GPIO_PIN_14
+#define L2_GPIO_Port GPIOB
+#define L3_Pin GPIO_PIN_15
+#define L3_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+typedef uint32_t  u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
+//IO�ڵ�ַӳ��
+#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C 
+#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C 
+#define GPIOC_ODR_Addr    (GPIOC_BASE+12) //0x4001100C 
+#define GPIOD_ODR_Addr    (GPIOD_BASE+12) //0x4001140C 
+#define GPIOE_ODR_Addr    (GPIOE_BASE+12) //0x4001180C 
+#define GPIOF_ODR_Addr    (GPIOF_BASE+12) //0x40011A0C    
+#define GPIOG_ODR_Addr    (GPIOG_BASE+12) //0x40011E0C    
+
+#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808 
+#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08 
+#define GPIOC_IDR_Addr    (GPIOC_BASE+8) //0x40011008 
+#define GPIOD_IDR_Addr    (GPIOD_BASE+8) //0x40011408 
+#define GPIOE_IDR_Addr    (GPIOE_BASE+8) //0x40011808 
+#define GPIOF_IDR_Addr    (GPIOF_BASE+8) //0x40011A08 
+#define GPIOG_IDR_Addr    (GPIOG_BASE+8) //0x40011E08 
+ 
+
+#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //��� 
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //���� 
+
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //��� 
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //���� 
+
+#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  //��� 
+#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  //���� 
+
+#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  //��� 
+#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  //���� 
+
+#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  //��� 
+#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  //����
+
+#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  //��� 
+#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  //����
+
+#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  //��� 
+#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  //����
+
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ 01_test/Core/Src/main.c
@@ -0,0 +1,289 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "dma.h"
+#include "i2c.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "oled.h"
+#include "bmp.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+#define FRAME_TARGET_MS 16  
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void OLED_ShowTimer(u8 x, u8 y, u32 total_seconds, u8 size, u8 mode);
+void OLED_ShowEncoderValue(void);
+
+uint8_t BMP3_rotated[72];
+u32 timer_seconds = 0 , timer_seconds_cont = 0;
+
+void OLED_ShowFPS(void)
+{
+    static uint32_t lastTick = 0;
+    static uint32_t frameCount = 0;
+    static uint32_t fps = 0;
+
+    frameCount++;
+
+    if (HAL_GetTick() - lastTick >= 1000) // 1秒过去了
+    {
+        fps = frameCount;
+        frameCount = 0;
+        lastTick = HAL_GetTick();
+
+        OLED_ShowString(0, 0, "FPS:", 12, 1);  // 显示前缀
+        OLED_ShowNum(24, 0, fps, 3, 12, 1);       // 显示FPS值
+    }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_I2C1_Init();
+  MX_TIM1_Init();
+  MX_USART1_UART_Init();
+  MX_ADC1_Init();
+  MX_TIM2_Init();
+  MX_SPI1_Init();
+  /* USER CODE BEGIN 2 */
+	HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_ALL);
+	OLED_Init();
+	OLED_DisPlay_On();
+	OLED_ColorTurn(0);
+  OLED_DisplayTurn(0);
+	printf("system_ok\r\n");
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	uint16_t fan_cont = 0, bat_lev_cont = 0 , temp = 0;
+  while (1)
+  {
+			uint32_t frame_start = HAL_GetTick();  // 记录帧开始时间
+
+			OLED_ShowFPS();
+			OLED_ShowEncoderValue();
+			OLED_ShowPicture(11,6+12,24,24,FAN_FRAMES[fan_cont],1);
+			OLED_ShowPicture(1+40,12+12,24,12,BAR_SOLID,1);
+			OLED_ShowPicture(28+40,12+12,24,12,BAR_SOLID,1);
+			OLED_ShowPicture(55+40,12+12,24,12,BAR_AIR,1);
+
+			if(bat_lev_cont % 20 == 0)
+			{
+					temp++;
+					temp %= 50;
+					OLED_ShowPicture(96,0,32,16,BAT_LEVELS[bat_lev_cont / 20],1);
+					OLED_ShowPicture(42,3,16,16,TEMP_ICO,1);
+					OLED_ShowNum(56,2,temp,2,16,1);
+					OLED_ShowPicture(72,4,12,12,TEMP_C,1);
+					bat_lev_cont %= 120;
+			}
+
+			if(timer_seconds_cont % 35 == 0)
+			{
+					timer_seconds++;
+					if(timer_seconds > 5999) timer_seconds = 0;
+					OLED_ShowTimer(10, 40, timer_seconds, 16, 1);
+					timer_seconds_cont = 0;
+			}
+
+			OLED_Refresh();
+
+			fan_cont++;
+			fan_cont %= 6;
+			bat_lev_cont++;
+			timer_seconds_cont++;
+
+			uint32_t frame_end = HAL_GetTick();  // 记录帧结束时间
+			uint32_t frame_time = frame_end - frame_start;
+
+			if (frame_time < FRAME_TARGET_MS)
+			{
+					HAL_Delay(FRAME_TARGET_MS - frame_time);  // 动态补偿延时
+			}
+//		HAL_Delay(1);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+void OLED_ShowTimer(u8 x, u8 y, u32 total_seconds, u8 size, u8 mode)
+{
+    u32 minutes = total_seconds / 60;
+    u32 seconds = total_seconds % 60;
+//		OLED_ShowChinese(x,y,11,16,1);
+//		OLED_ShowChinese(x+17,y,12,16,1);
+		OLED_ShowPicture(x,y,24,24,TIMER1,1);
+    OLED_ShowNum(x+28,y+4, minutes, 2, size, mode); // ��ʾ���ӣ�2λ��
+    OLED_ShowString(x + size * 2 * 1-16+28, y-1+4, ":", size, mode); // ��ʾð�ţ�6 ���ַ����ϵ��
+    OLED_ShowNum(x + size * 2 * 1-8+28, y+4, seconds, 2, size, mode); // ��ʾ�루2λ��
+}
+void OLED_ShowEncoderValue(void)
+{
+    int count = __HAL_TIM_GET_COUNTER(&htim1);
+    OLED_ShowString(84, 52, "CNT:", 12, 1);         // 显示前缀
+    OLED_ShowNum(110, 52, (int)count/4, 3, 12, 1);            // 显示编码器数值
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
